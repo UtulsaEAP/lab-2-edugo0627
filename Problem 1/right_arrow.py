ddc6929b415a7ee94d7bedde2f30cf9252c7b27a
edugo0627 <ejv0065@utulsa.edu>
2024-02-08 13:53:01 -0600

    i dont know anymore

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -7,7 +7,6 @@ def right_arrow():
     
     row2 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char
     row3 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char + head_char
-    
     print(row1)
     print(row2)
     print(row3)
