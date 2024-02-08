968ef19ba6660cca2c67fc35fbfc86d927cfd801
edugo0627 <ejv0065@utulsa.edu>
2024-02-08 12:04:26 -0600

    problem 1 changes

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -11,10 +11,10 @@ def right_arrow():
     row1 = '      ' + head_char
     ''' Type your code here. '''
     
-    base_char = input('Enter character for base of arrow:  ')
-    head_char = input('Enter character for the top of arrow: ')
+    base_char = input("Enter character for base of arrow: ")
+    head_char = input("Enter character for the top of arrow: ")
 
-    row1 = "      " + head_char
+    row1 = '      ' + head_char
     row2 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char
     row3 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char + head_char
     
