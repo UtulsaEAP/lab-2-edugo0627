79d41dd7ed4f11c87dda506492d44e5e7446df38
edugo0627 <ejv0065@utulsa.edu>
2024-02-08 16:46:13 -0600

    change to 3?

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -11,9 +11,9 @@ def caffeine():
     hours_12 = float(caffeine_mg)*.5**(12/6)
     hours_24 = float(caffeine_mg)*.5**(24/6)
 
-    print('     After 6 hours: ' + f'{hours_6:.2f}'+ ' mg')
-    print('     After 12 hours: ' + f'{hours_12:.2f}' + ' mg')
-    print('     After 24 hours: ' + f'{hours_24:.2f}' + ' mg')
+    print(' After 6 hours: ' + f'{hours_6:.2f}'+ ' mg')
+    print(' After 12 hours: ' + f'{hours_12:.2f}' + ' mg')
+    print(' After 24 hours: ' + f'{hours_24:.2f}' + ' mg')
     
     
 if __name__ == "__main__":
