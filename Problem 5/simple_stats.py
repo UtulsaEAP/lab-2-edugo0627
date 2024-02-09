164692adfa3090ba349458bc0f51255574f82d49
edugo0627 <ejv0065@utulsa.edu>
2024-02-08 19:50:59 -0600

    problem 5

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -1,9 +1,21 @@
-
+'''
+Lab 2 problem 5:
+Name: Emma J. Verdugo
+lab: @2pm
+'''
 def simple_stats():
     num1 = float(input())
     num2 = float(input())
     num3 = float(input())
     num4 = float(input())
     ''' Type your code here. '''
+    
+    product = num1 * num2 * num3 * num4
+    avg = (num1 + num2 + num3 + num4) / 4 
+    
+    print(f'{product:.0f}',f'{avg:.0f}')
+    print(f'{product:.3f}',f'{avg:.3f}')
+    
+    
 if __name__ == "__main__":
     simple_stats()
